9109360e6442b16cfc31cafafb824c536c4cde68
a.karlov <a.karlov@rbkmoney.com>
2018-12-24 12:02:35 +0300

    init

---
+++ src/main/java/com/rbkmoney/fistfulreporter/FistfulReporterApplication.java
@@ -0,0 +1,13 @@
+package com.rbkmoney.fistfulreporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FistfulReporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FistfulReporterApplication.class, args);
+    }
+
+}
