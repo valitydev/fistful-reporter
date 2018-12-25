2e776ff57073c375ea6dfca1c356e0192b9ad6eb
a.karlov <a.karlov@rbkmoney.com>
2018-12-25 14:59:03 +0300

    add jooq and flyway

--- src/main/java/com/rbkmoney/fistfulreporter/FistfulReporterApplication.java
+++ src/main/java/com/rbkmoney/fistfulreporter/FistfulReporterApplication.java
@@ -3,7 +3,7 @@ package com.rbkmoney.fistfulreporter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.rbkmoney.fistfulreporter"})
 public class FistfulReporterApplication {
 
     public static void main(String[] args) {
