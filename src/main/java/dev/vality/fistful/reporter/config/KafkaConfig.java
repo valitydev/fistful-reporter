f1bba3faeb98dfbab158eb3f8f41779aaf446a8c
Anatoly Karlov <karleowne@gmail.com>
2022-09-06 17:37:54 +0700

    TD-315: add iam auth

--- src/main/java/dev/vality/fistful/reporter/config/KafkaConfig.java
+++ src/main/java/dev/vality/fistful/reporter/config/KafkaConfig.java
@@ -1,11 +1,8 @@
 package dev.vality.fistful.reporter.config;
 
-import dev.vality.fistful.reporter.kafka.serdes.SinkEventDeserializer;
 import dev.vality.machinegun.eventsink.MachineEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -15,8 +12,6 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.SeekToCurrentBatchErrorHandler;
-
-import java.util.Map;
 
 @Slf4j
 @Configuration
@@ -61,18 +56,11 @@ public class KafkaConfig {
 
     private ConcurrentKafkaListenerContainerFactory<String, MachineEvent> listenerContainerFactory() {
         var factory = new ConcurrentKafkaListenerContainerFactory<String, MachineEvent>();
-        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(consumerConfig()));
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(kafkaProperties.buildConsumerProperties()));
         factory.setConcurrency(consumerConcurrency);
         factory.setBatchErrorHandler(new SeekToCurrentBatchErrorHandler());
         factory.setBatchListener(true);
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         return factory;
-    }
-
-    private Map<String, Object> consumerConfig() {
-        var properties = kafkaProperties.buildConsumerProperties();
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SinkEventDeserializer.class);
-        return properties;
     }
 }
