350750c2fd923284dd91fc482b21301a0013dd16
Anatoly Karlov <karleowne@gmail.com>
2022-09-07 12:50:49 +0700

    TD-315: add iam auth

--- src/main/java/dev/vality/fistful/reporter/config/KafkaConfig.java
+++ src/main/java/dev/vality/fistful/reporter/config/KafkaConfig.java
@@ -1,9 +1,12 @@
 package dev.vality.fistful.reporter.config;
 
+import dev.vality.fistful.reporter.kafka.serdes.SinkEventDeserializer;
 import dev.vality.kafka.common.util.ExponentialBackOffDefaultErrorHandlerFactory;
 import dev.vality.machinegun.eventsink.MachineEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
+
+import java.util.Map;
 
 @Slf4j
 @Configuration
@@ -56,11 +61,18 @@ public class KafkaConfig {
 
     private ConcurrentKafkaListenerContainerFactory<String, MachineEvent> listenerContainerFactory() {
         var factory = new ConcurrentKafkaListenerContainerFactory<String, MachineEvent>();
-        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(kafkaProperties.buildConsumerProperties()));
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(getConfigs()));
         factory.setConcurrency(consumerConcurrency);
         factory.setCommonErrorHandler(ExponentialBackOffDefaultErrorHandlerFactory.create());
         factory.setBatchListener(true);
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         return factory;
+    }
+
+    private Map<String, Object> getConfigs() {
+        var properties = kafkaProperties.buildConsumerProperties();
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SinkEventDeserializer.class);
+        return properties;
     }
 }
